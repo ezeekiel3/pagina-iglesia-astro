26c8e569fa93556a1cfcc040af63d2beaad08445
ezeekiel3 <lolochapa10@hotmail.com>
2025-01-14 15:13:04 -0300

    ahora el boton de conocenos funciona y se esta arreglando los otros botones

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,9 @@
 import { atom } from 'nanostores'
 
-export const showInformacion = atom(false)
+export const showConocenos = atom(false)
+
+export const showCreencias = atom(false)
+
+export const estado = atom(0)
+
+export const estados = ['show_conocenos', 'show_conocenos2', 'show_conocenos3', 'show_conocenos4']
