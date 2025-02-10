d2437d60ad4e0d7ff4e2c66f67ea25d138e6c459
ezeekiel3 <lolochapa10@hotmail.com>
2025-02-10 12:22:42 -0300

    se agrego el header para moviles

--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -1,24 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import menuImg from '../img/menu.png'
+import closeImg from '../img/close.png'
+import arrowRight from '../img/arrowRight.png'
 
-type HeaderButtonProps = { buttonText: string; children?: React.ReactNode }
+type HeaderButtonProps = { buttonText?: string; children?: React.ReactNode }
 
 export default function HeaderButton({ buttonText, children }: HeaderButtonProps) {
     const [hoveringButton, setHoveringButton] = useState(false)
     const [hoveringDropdown, setHoveringDropdown] = useState(false)
+    const [openMenu, setOpenMenu] = useState(false)
 
     children = children && !Array.isArray(children) ? [children] : children
 
+    useEffect(() => {
+        if (openMenu) {
+            document.body.classList.add('overflow-hidden')
+        } else {
+            document.body.classList.remove('overflow-hidden')
+        }
+
+        return () => {
+            document.body.classList.remove('overflow-hidden')
+        }
+    }, [openMenu])
+
     return (
-        <div className='relative'>
+        <div className='xl:relative xl:block flex'>
             <button
-                className='text-xl text-logo transition-colors duration-200 hover:text-orange-300'
+                className='text-xl xl:flex hidden text-logo transition-colors duration-200 hover:text-orange-300'
                 onClick={() => setHoveringButton(true)}
                 onMouseEnter={() => setHoveringButton(true)}
                 onMouseLeave={() => setHoveringButton(false)}>
                 {buttonText}
             </button>
+            <button className='xl:hidden flex' onClick={() => setOpenMenu(true)}>
+                <img src={menuImg.src} alt='' className='h-12' />
+            </button>
             <ul
-                className={`absolute min-w-full bg-white rounded-md overflow-hidden transition-all duration-200 shadow text-logo
+                className={`absolute min-w-full xl:flex hidden flex-col bg-white rounded-md overflow-hidden transition-all duration-200 shadow text-logo
                 ${
                     children && (hoveringButton || hoveringDropdown)
                         ? 'pointer-events-auto opacity-100'
@@ -28,6 +47,47 @@ export default function HeaderButton({ buttonText, children }: HeaderButtonProps
                 onMouseLeave={() => setHoveringDropdown(false)}>
                 {children}
             </ul>
+            <div
+                onClick={() => setOpenMenu(false)}
+                className={`xl:hidden fixed inset-0 z-20 bg-gray-900 bg-opacity-50 transition-opacity duration-300 ${
+                    openMenu ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+                }`}></div>
+
+            <div
+                className={`xl:hidden fixed top-0 z-20 right-0 h-full w-64 bg-orange-100 shadow-lg transform transition-transform duration-300 ${
+                    openMenu ? 'translate-x-0' : 'translate-x-full'
+                }`}>
+                <div className='p-4 h-full'>
+                    <button onClick={() => setOpenMenu(false)}>
+                        <img src={closeImg.src} alt='' className='h-10' />
+                    </button>
+                    <nav className='h-2/3'>
+                        <ul className='flex items-center justify-center text-xl text-logo gap-8 h-full flex-col'>
+                            <li className='flex items-center'>
+                                <button className='hover:text-orange-300 transition-all duration-300'>Iglesia</button>
+                                <img src={arrowRight.src} alt='' className='h-6' />
+                            </li>
+                            <li className='flex items-center'>
+                                <button className='hover:text-orange-300 transition-all duration-300'>Material</button>
+                                <img src={arrowRight.src} alt='' className='h-6' />
+                            </li>
+                            <li className='flex items-center'>
+                                <button className='hover:text-orange-300 transition-all duration-300'>Niños</button>
+                                <img src={arrowRight.src} alt='' className='h-6' />
+                            </li>
+                            <li className='flex items-center'>
+                                <button className='hover:text-orange-300 transition-all duration-300'>
+                                    Centro de Desarrollo
+                                </button>
+                                <img src={arrowRight.src} alt='' className='h-6' />
+                            </li>
+                            <li>
+                                <button className='hover:text-orange-300 transition-all duration-300'>Contacto</button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }
