c774537707b5307ccdd8ce477e13cc01a4271d6d
chapuwu <lolochapa10@hotmail.com>
2025-10-19 13:06:21 -0300

    este componente ahora puede mostrar cualquier contenido

--- src/components/SeccionBiblica.tsx
+++ src/components/SeccionBiblica.tsx
@@ -6,49 +6,25 @@ type SeccionBiblicaProps = {
     titleSection: string
     descriptionSection: string
     cantidadMaterial: string
+    emoji: string
+    contenido: { id: string; name: string; icon: string }[]
 }
-
-const nuevoTestamento = [
-    { id: 'evangelio', name: 'Los Cuatro Evangelios', icon: 'folder' },
-    { id: 'hechos', name: 'Hechos', icon: 'folder' },
-    { id: 'romanos', name: 'Romanos', icon: 'folder' },
-    { id: 'corintios', name: 'Cartas de los Corintios', icon: 'folder' },
-    { id: 'galatas', name: 'Galatas', icon: 'folder' },
-    { id: 'efesios', name: 'Efesios', icon: 'folder' },
-    { id: 'filipenses', name: 'Filipenses', icon: 'folder' },
-    { id: 'colosenses', name: 'Colosenses', icon: 'folder' },
-    { id: 'tesalonica', name: 'Cartas de Tesalonica', icon: 'folder' },
-    { id: 'timoteo', name: 'Cartas a Timoteo', icon: 'folder' },
-    { id: 'tito', name: 'Tito', icon: 'folder' },
-    { id: 'filemon', name: 'Filemon', icon: 'folder' },
-    { id: 'hebreos', name: 'Hebreos', icon: 'folder' },
-    { id: 'santiago', name: 'Santiago', icon: 'folder' },
-    { id: 'pedro', name: 'Cartas de Pedro', icon: 'folder' },
-    { id: 'juan', name: 'Cartas de Juan', icon: 'folder' },
-    { id: 'judas', name: 'Judas', icon: 'folder' },
-    { id: 'apocalipsis', name: 'Apocalipsis', icon: 'folder' },
-]
-
-const nombresJesus = [
-    { id: 'salvador', name: 'Jesus, El Salvador.', icon: 'file' },
-    { id: 'senior', name: 'Jesus, El Señor', icon: 'file' },
-    { id: 'maestro', name: 'Jesus, El Maestro', icon: 'file' },
-    { id: 'profeta', name: 'Jesus, El Profeta', icon: 'file' },
-]
 
 export default function SeccionBiblica({
     section,
     titleSection,
     descriptionSection,
     cantidadMaterial,
+    emoji,
+    contenido,
 }: SeccionBiblicaProps) {
     const [showMore, setShowMore] = useState(false)
 
     return (
         <div className='flex items-center flex-col mt-10 bg-orange-200 h-auto pb-4 w-2/3 rounded-2xl border-opacity-30 shadow-md border-logo border-2'>
             <div className='bg-orange-100 bg-opacity-70 mt-7 mb-7 h-1/5 w-full flex flex-row items-center pl-7'>
-                <p className='text-4xl'>📖</p>
-                <div className='ml-5 flex flex-col gap-2 h-auto'>
+                <p className='text-4xl'>{emoji}</p>
+                <div className='ml-5 flex flex-col gap-2 h-auto py-5'>
                     <p className='opacity-50 font-sans'>Seccion {section}</p>
                     <h2 className='text-2xl text-logo font-semibold opacity-90'>{titleSection}</h2>
                     <p className='opacity-65'>{descriptionSection}</p>
@@ -56,10 +32,10 @@ export default function SeccionBiblica({
             </div>
             <div className='w-full flex items-center flex-col gap-4'>
                 {showMore
-                    ? nuevoTestamento.map((element) => (
+                    ? contenido.map((element) => (
                           <SectionItem key={element.id} itemSection={element} showMore={showMore} />
                       ))
-                    : nuevoTestamento.map((element, i) => {
+                    : contenido.map((element, i) => {
                           if (i < 3) {
                               return <SectionItem key={element.id} itemSection={element} showMore={true} />
                           }
