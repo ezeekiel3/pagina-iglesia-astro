f1f3d9fce0c9bcf88f898fefe037f1ea64f73f3a
chapuwu <lolochapa10@hotmail.com>
2025-10-23 18:50:17 -0300

    se arreglo que al darle click a los pdf se abria como una carpeta, tambien se arreglo que los item pdf no se abrian

--- src/components/SectionItem.tsx
+++ src/components/SectionItem.tsx
@@ -6,6 +6,7 @@ type SectionItemProps = {
         name: string
         icon: string
         pdfs?: { versiculo: string; filePath: string }[]
+        pdfPath?: string
     }
     showMore: boolean
 }
@@ -18,7 +19,13 @@ export default function SectionItem({ itemSection, showMore }: SectionItemProps)
             className={`border border-logo hover:bg-logo hover:bg-opacity-10 duration-100 border-opacity-30 rounded-lg h-min w-11/12 ${
                 showMore ? 'visible' : 'invisible'
             }`}>
-            <div className='flex flex-row pl-7 gap-3 py-4 items-center' onClick={() => setOpenFolder(!openFolder)}>
+            <div
+                className={`flex flex-row pl-7 gap-3 ${itemSection.icon === 'folder' ? 'py-4' : ''} items-center`}
+                onClick={() => {
+                    if (itemSection.icon === 'folder') {
+                        setOpenFolder(!openFolder)
+                    }
+                }}>
                 {itemSection.icon === 'folder' ? (
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
@@ -38,7 +45,16 @@ export default function SectionItem({ itemSection, showMore }: SectionItemProps)
                         <path d='M360-460h40v-80h40q17 0 28.5-11.5T480-580v-40q0-17-11.5-28.5T440-660h-80v200Zm40-120v-40h40v40h-40Zm120 120h80q17 0 28.5-11.5T640-500v-120q0-17-11.5-28.5T600-660h-80v200Zm40-40v-120h40v120h-40Zm120 40h40v-80h40v-40h-40v-40h40v-40h-80v200ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z' />
                     </svg>
                 )}
-                <p className='xl:text-xl text-lg text-logo font-semibold'>{itemSection.name}</p>
+                {itemSection.icon === 'folder' ? (
+                    <p className='xl:text-xl text-lg text-logo font-semibold'>{itemSection.name}</p>
+                ) : (
+                    <a
+                        href={itemSection.pdfPath}
+                        className='xl:text-xl text-lg text-logo font-semibold w-full h-full py-4'
+                        target='_blank'>
+                        {itemSection.name}
+                    </a>
+                )}
                 {itemSection.icon === 'folder' ? (
                     openFolder ? (
                         <svg
