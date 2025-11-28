ffb8137c5d955ac7c73f026626bbb83bd95b87c3
ezeekiel3 <lolochapa10@hotmail.com>
2025-11-28 09:39:40 -0300

    ahora devuelve las secciones sin contenido

--- src/db/getSections.ts
+++ src/db/getSections.ts
@@ -2,12 +2,7 @@ import { db } from './index'
 import { sections, items, pdfs } from './schema'
 
 export async function getSections() {
-    const sectionsResult = await db
-        .select({
-            titleSection: sections.title,
-            descriptionSection: sections.description,
-            emoji: sections.emoji,
-        })
-        .from(sections)
-    console.log(sectionsResult)
+    const sectionsResult = await db.select().from(sections)
+
+    return sectionsResult
 }
